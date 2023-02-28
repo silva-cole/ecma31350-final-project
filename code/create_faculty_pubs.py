90d7cfda1b836159764556807ed5fabdd1b09488
Kavitya Sarma <kavitya@uchicago.edu>
2023-02-28 12:05:08 -0600

    adds code to clean the scraped data

--- code/create_faculty_pubs.py
+++ code/create_faculty_pubs.py
@@ -6,6 +6,7 @@ import pandas as pd
 from scholarly import scholarly
 from tqdm import tqdm
 import time
+import random 
 
 data = []
 
@@ -14,20 +15,22 @@ faculty_df = pd.read_csv('../data/Surnames_faculty_data.csv')
 names = faculty_df['fname'] + " " + faculty_df['lname']
 
 #for i in tqdm(range(len(names))):
-for j in tqdm(range(2)):
-    for i in range(4):
-        search_results = scholarly.search_author(names[j*5+i])
-        author = scholarly.fill(next(search_results))
+#for j in tqdm(range(611)):
+for i in tqdm(range(10)):
+    search_results = scholarly.search_author(names[i])
+    author = scholarly.fill(next(search_results))
 
-        publications = author['publications']
+    publications = author['publications']
 
-        for pub in publications:
-            bib = pub['bib']
-            title = bib['title']
-            journal = bib['citation']
-            year = journal[-4:]
-            citations = pub['num_citations']
-            data.append([names[i],title,journal,year,citations])
+    for pub in publications:
+        bib = pub['bib']
+        title = bib['title']
+        journal = bib['citation']
+        year = journal[-4:]
+        citations = pub['num_citations']
+        data.append([names[i],title,journal,year,citations])
+
+    time.sleep(random.randint(1,10))
 
 
 faculty_pubs = pd.DataFrame(data)
